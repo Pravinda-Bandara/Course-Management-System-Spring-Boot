4cef65a47882f09ab4c5066f11232a52066eac8a
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-12-20 05:29:08 +0530

    add data jpa and moddle mapper

--- src/main/java/lk/hmpb/course/AppInitializer.java
+++ src/main/java/lk/hmpb/course/AppInitializer.java
@@ -1,10 +1,17 @@
 package lk.hmpb.course;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppInitializer {
+
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppInitializer.class, args);
