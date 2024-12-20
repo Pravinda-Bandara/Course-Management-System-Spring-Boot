e1a51aa1eff6ced35bec1221c057e5329ca59fbf
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-12-20 05:31:12 +0530

    login and register user tos

---
+++ src/main/java/lk/hmpb/course/to/UserLoginReqTo.java
@@ -0,0 +1,21 @@
+package lk.hmpb.course.to;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginReqTo {
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+}
---
+++ src/main/java/lk/hmpb/course/to/UserRegisterReqTo.java
@@ -0,0 +1,26 @@
+package lk.hmpb.course.to;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisterReqTo {
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+    @NotBlank(message = "Number is mandatory")
+    private String number;
+}
