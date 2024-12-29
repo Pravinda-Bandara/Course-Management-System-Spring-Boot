a31f8a0b9cebbd9b29a2fbf6efd33e4585bd685d
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-12-30 01:35:00 +0530

    add cors

--- src/main/java/lk/hmpb/course/controller/AdminController.java
+++ src/main/java/lk/hmpb/course/controller/AdminController.java
@@ -1,11 +1,13 @@
 package lk.hmpb.course.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin")
+@CrossOrigin
 public class AdminController {
 
     @GetMapping("/data")
--- src/main/java/lk/hmpb/course/controller/CourseHttpController.java
+++ src/main/java/lk/hmpb/course/controller/CourseHttpController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
+@CrossOrigin
 public class CourseHttpController {
 
     private final CourseService courseService;
