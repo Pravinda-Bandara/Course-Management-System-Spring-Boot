9c968c14a102659b0234e0c38c427b14389ea0cc
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-12-30 15:56:27 +0530

    change user Interface for get all users

--- src/main/java/lk/hmpb/course/service/UserService.java
+++ src/main/java/lk/hmpb/course/service/UserService.java
@@ -1,11 +1,13 @@
 package lk.hmpb.course.service;
 
 import lk.hmpb.course.entiry.User;
+import lk.hmpb.course.to.AllUserResponseTo;
 import lk.hmpb.course.to.UserLoginReqTo;
 import lk.hmpb.course.to.UserRegisterReqTo;
 import lk.hmpb.course.to.UserResponseTo;
 import lk.hmpb.course.util.ApiResponse;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
@@ -13,4 +15,5 @@ public interface UserService {
     boolean existsByEmail(String email);
     ApiResponse<UserResponseTo> register(UserRegisterReqTo userRegisterReqTo);
     ApiResponse<UserResponseTo>  login(UserLoginReqTo userLoginReqTo);
+    ApiResponse<List<AllUserResponseTo>> getAllUsers(String token);
 }
